b914acf62009d47c463a46292e5fc35a95f34ff8
sajid365-sr <sajidsorker86@gmail.com>
2022-11-07 03:47:42 +0600

    first commit getting user from server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUser] = useState('');
+  useEffect(() =>{
+    fetch('http://localhost:5000/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  }, [users]);
+
+
+  const handleAddUser = (event) =>{
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <form onSubmit={handleAddUser}>
+      <input type="text" name='name' />
+      <br />
+      <input type="email" name='email' />
+      <br />
+      <input type="submit" value='Submit' />
+    </form>
+
+      <h2>Users: {users.length}</h2>
+      <div>
+        {
+          users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
+        }
+      </div>
     </div>
   );
 }
