6c07c230d0331d1cf8c68c5f585847a19c4bfc1a
sajid365-sr <sajidsorker86@gmail.com>
2022-11-07 04:24:07 +0600

    sending data to the backend using fetch method

--- src/App.js
+++ src/App.js
@@ -9,20 +9,38 @@ function App() {
     fetch('http://localhost:5000/users')
     .then(res => res.json())
     .then(data => setUser(data))
-  }, [users]);
+  }, []);
 
 
   const handleAddUser = (event) =>{
     event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name,email};
+    console.log(user);
+    
+
+    fetch('http://localhost:5000/users',{
+      method:'POST',
+      headers:{
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error))
+
+    event.target.reset();
   }
 
   return (
     <div className="App">
 
     <form onSubmit={handleAddUser}>
-      <input type="text" name='name' />
+      <input type="text" name='name' placeholder='name' />
       <br />
-      <input type="email" name='email' />
+      <input type="email" name='email' placeholder='email' />
       <br />
       <input type="submit" value='Submit' />
     </form>
